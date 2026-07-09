a9eb1b75afacbbaac869d8f428781ff255a678c1
Gpcode233 <godspowerojini8@gmail.com>
2026-07-09 23:32:05 +0100

    implement hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react"
+
+const Hero = () => {
+    const videoRef = useRef();
+
+    useEffect(() => {
+        if (videoRef.current) videoRef.current.playbackRate = 2;
+    }, []);
+
+  return (
+    <section id="hero">
+        <div>
+            <h1>Macbook Pro</h1>
+            <img src="/title.png" alt="Macbook title" />
+            <p>Supercharged by M5 Pro and M5 Max.</p>
+        </div>
+
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+
+        <button>Buy</button>
+        <p>From $1,599 or $133.25/mo. for 12 mo.* </p>
+    </section>
+  )
+}
+
+export default Hero
